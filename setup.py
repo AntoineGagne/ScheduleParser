61a66ec0b69e542e1deb26555098f15902018566
Antoine Gagne <gagnantoine@gmail.com>
2015-10-15 16:21:49 -0400

    Adds setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import ez_setup
+
+#install setuptools
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name = 'ScheduleParser',
+      version = '1.0',
+      description = 'Export the Universite Laval schedule in a .csv file.',
+      author = 'Antoine Gagne',
+      author_email = 'antoine.gagne.2@ulaval.ca',
+      packages = find_packages(),
+
+      install_requires = ['beautifulsoup4>=4.4.1',
+                          'requests>=2.8.1'],
+     )
